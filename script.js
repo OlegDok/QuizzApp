bbb0938c4fc4e7acef1cde2a7056e369c395ea34
OlegDok <DoktorOleg@outlook.de>
2023-12-13 20:04:36 +0100

    audio and success background

--- script.js
+++ script.js
@@ -21,41 +21,85 @@ let questions =[
         "answer_2":"Elektron",
         "answer_3":"Neutron",
         "answer_4":"Quark",
-        "right_answer":3,
+        "right_answer":4,
     }
     
 
 
 ];
+let rightQuesions=0;
 let currentQuestion=0;
+let AUDIO_SUCCESS = new Audio(`audio/success.mp3`);
+let AUDIO_FAIL = new Audio(`audio/error.mp3`);
+let AUDIO_FINISH = new Audio(`audio/finish.mp3`);
 
 function init(){
     questionsArrayLen();
     showQuestion();
+
 }
 function questionsArrayLen(){
     document.getElementById("all-questions").innerHTML=questions.length;
 }
+
 function showQuestion(){
+    if (currentQuestion >= questions.length) {
+        document.getElementById("endScreen").style="";
+        document.getElementById("quiz-body").style="display:none";
+
+        document.getElementById("amountOfQuestions").innerHTML = questions.length;
+        document.getElementById("amountCorrectAnswers").innerHTML =  rightQuesions;
+        AUDIO_FINISH.play();
+    } else{
     let question = questions[currentQuestion];
-    
+    document.getElementById("question-number").innerHTML=currentQuestion + 1;
     document.getElementById('question-text').innerHTML = question["question"];
     document.getElementById('answer_1').innerHTML = question["answer_1"];
     document.getElementById('answer_2').innerHTML = question["answer_2"];
     document.getElementById('answer_3').innerHTML = question["answer_3"];
     document.getElementById('answer_4').innerHTML = question["answer_4"];
-
+    }
 }
 function answer(selection){
     let question = questions[currentQuestion];
-    console.log("Selected answer is " , selection);
     let selectedQuestionNumber = selection.slice(-1);
-    console.log("selectedQuestionNumber is" , selectedQuestionNumber);
-    console.log("Current Question is " , question["right_answer"]);
+    let idOfRightAnswer = `answer_${question["right_answer"]}`;
 
     if (selectedQuestionNumber == question ["right_answer"]) {
-        console.log("Richtige Antwort!");
+        document.getElementById(selection).parentNode.classList.add("bg-success");
+        rightQuesions ++;
+        AUDIO_SUCCESS.play();
     } else {
-        console.log("Falsche Antwort!");
+        document.getElementById(selection).parentNode.classList.add("bg-danger");
+        document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+        AUDIO_FAIL.play();
     }
+    document.getElementById("next-button").disabled = false;
+}
+
+function nextQuestion(){
+    currentQuestion++;
+    document.getElementById("next-button").disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+    
+}
+
+function resetAnswerButtons(){
+    document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_4").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+}
+
+function restartGame(){
+    rightQuesions =0;
+    currentQuestion =0;
+    document.getElementById("endScreen").style="display:none";
+    document.getElementById("quiz-body").style="";
+    init();
 }
