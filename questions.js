3ea157a64238d22f6aab443ce1c59b3fd6c74e44
OlegDok <DoktorOleg@outlook.de>
2023-12-18 20:13:06 +0100

    sortet and add sport

---
+++ questions.js
@@ -0,0 +1,178 @@
+
+let historyQuestions = [
+    {
+      question: "In welchem Jahr endete der Zweite Weltkrieg?",
+      answer_1: "1943",
+      answer_2: "1945",
+      answer_3: "1950",
+      answer_4: "1939",
+      right_answer: 2,
+    },
+    {
+      question: "Wer war der erste Präsident der Vereinigten Staaten?",
+      answer_1: "George Washington",
+      answer_2: "Thomas Jefferson",
+      answer_3: "Abraham Lincoln",
+      answer_4: "John Adams",
+      right_answer: 1,
+    },
+    {
+      question: "In welchem Jahr endete der Erste Weltkrieg?",
+      answer_1: "1914",
+      answer_2: "1917",
+      answer_3: "1918",
+      answer_4: "1920",
+      right_answer: 3,
+    },
+    {
+      question: "Welcher berühmte Herrscher regierte im antiken Rom und wurde am 15. März 44 v. Chr. ermordet?",
+      answer_1: "Julius Caesar",
+      answer_2: "Alexander der Große",
+      answer_3: "Augustus",
+      answer_4: "Nero",
+      right_answer: 1,
+    },
+    {
+      question: "Was war die Magna Carta?",
+      answer_1: "Ein Vertrag zwischen Deutschland und Frankreich",
+      answer_2: "Ein mittelalterliches Grundlagendokument",
+      answer_3: "Ein antikes Handelsschiff",
+      answer_4: "Ein berühmtes Gemälde der Renaissance",
+      right_answer: 2,
+    },
+    {
+      question: "Welches Ereignis führte zur Französischen Revolution?",
+      answer_1: "Der Schwarze Tod",
+      answer_2: "Die Entdeckung Amerikas",
+      answer_3: "Die Hinrichtung Ludwig XVI.",
+      answer_4: "Der Sturz der Berliner Mauer",
+      right_answer: 3,
+    }
+  ];
+  
+  let scienceQuestions = [
+    {
+      question: "Welche Einheit misst die Intensität von Erdbeben?",
+      answer_1: "Watt",
+      answer_2: "Joule",
+      answer_3: "Hertz",
+      answer_4: "Richter",
+      right_answer: 4,
+      topic: "science",
+    },
+    {
+      question: "Was ist die chemische Formel für Wasser?",
+      answer_1: "CO2",
+      answer_2: "H2O",
+      answer_3: "O2",
+      answer_4: "CH4",
+      right_answer: 2,
+      topic: "science",
+    },
+    {
+      question: "Welches Gas macht etwa 78% der Erdatmosphäre aus?",
+      answer_1: "Sauerstoff",
+      answer_2: "Stickstoff",
+      answer_3: "Kohlendioxid",
+      answer_4: "Wasserstoff",
+      right_answer: 2,
+      topic: "science",
+    },
+    {
+      question: "Was ist die kleinste Einheit eines chemischen Elements?",
+      answer_1: "Molekül",
+      answer_2: "Atom",
+      answer_3: "Isotop",
+      answer_4: "Ion",
+      right_answer: 2,
+      topic: "science",
+    },
+    {
+      question: "Welcher Planet ist der dritte in unserem Sonnensystem?",
+      answer_1: "Mars",
+      answer_2: "Venus",
+      answer_3: "Erde",
+      answer_4: "Jupiter",
+      right_answer: 3,
+      topic: "science",
+    },
+  ];
+  
+  let sportQuestions =[
+    
+    {
+      question: "Wo fand die Fußball-Weltmeisterschaft 2018 statt?",
+      answer_1: "Deutschland",
+      answer_2: "Frankreich",
+      answer_3: "Brasilien",
+      answer_4: "Russland",
+      right_answer: 4,
+    },
+    {
+      question: "Wer hält den Weltrekord im Marathonlauf?",
+      answer_1: "Usain Bolt",
+      answer_2: "Eliud Kipchoge",
+      answer_3: "Mo Farah",
+      answer_4: "Haile Gebrselassie",
+      right_answer: 2,
+    },
+    {
+      question: "Welches Team gewann die NBA-Meisterschaft im Jahr 2021?",
+      answer_1: "Los Angeles Lakers",
+      answer_2: "Golden State Warriors",
+      answer_3: "Miami Heat",
+      answer_4: "Milwaukee Bucks",
+      right_answer: 4,
+    },
+    {
+      question: "Welche Sportart wird im 'Squared Circle' ausgetragen?",
+      answer_1: "Boxen",
+      answer_2: "Wrestling",
+      answer_3: "MMA (Mixed Martial Arts)",
+      answer_4: "Sumo-Ringen",
+      right_answer: 1,
+    },
+    {
+      question: "Wer hält den Rekord für die meisten Tore in der Geschichte der FIFA-Weltmeisterschaft?",
+      answer_1: "Lionel Messi",
+      answer_2: "Pelé",
+      answer_3: "Cristiano Ronaldo",
+      answer_4: "Miroslav Klose",
+      right_answer: 4,
+    }
+  ];
+  
+  let geoQuesion =[
+    {
+      question: "Was ist der größte Kontinent der Erde?",
+      answer_1: "Europa",
+      answer_2: "Afrika",
+      answer_3: "Asien",
+      answer_4: "Nordamerika",
+      right_answer: 3,
+    },
+    {
+      question: "Welcher Fluss ist der längste auf der Erde?",
+      answer_1: "Nil",
+      answer_2: "Amazonas",
+      answer_3: "Jangtse",
+      answer_4: "Mississippi",
+      right_answer: 2,
+    },
+    {
+      question: "Welcher Ozean liegt zwischen Nordamerika und Europa?",
+      answer_1: "Atlantischer Ozean",
+      answer_2: "Pazifischer Ozean",
+      answer_3: "Indischer Ozean",
+      answer_4: "Südlicher Ozean",
+      right_answer: 1,
+    },
+    {
+      question: "Welches Land hat die meisten Einwohner auf der Welt?",
+      answer_1: "Indien",
+      answer_2: "China",
+      answer_3: "USA",
+      answer_4: "Russland",
+      right_answer: 2,
+    },
+  ];
--- script.js
+++ script.js
@@ -1,48 +1,65 @@
-let isOnSciencePage = false;
-let questions = [
-  {
-    question: `Welches Element ist chemisch als "Au" bekannt?`,
-    answer_1: "Silber",
-    answer_2: "Gold",
-    answer_3: "Eisen",
-    answer_4: "Kupfer",
-    right_answer: 2,
-  },
-  {
-    question: `Welcher Planet ist der vierte in unserem Sonnensystem?`,
-    answer_1: "Venus",
-    answer_2: "Mars",
-    answer_3: "Jupiter",
-    answer_4: "Saturn",
-    right_answer: 2,
-  },
-  {
-    question: `Welche der folgenden Partikel ist kleiner als ein Atom?`,
-    answer_1: "Proton",
-    answer_2: "Elektron",
-    answer_3: "Neutron",
-    answer_4: "Quark",
-    right_answer: 4,
-  },
-];
-
+let isOnPage = 0;
 let rightQuestion = 0;
 let currentQuestion = 0;
+
 let AUDIO_SUCCESS = new Audio(`audio/success.mp3`);
 let AUDIO_FAIL = new Audio(`audio/error.mp3`);
 let AUDIO_FINISH = new Audio(`audio/finish.mp3`);
 
+
 function init() {
-  questionsArrayLen();
-  showQuestion();
+  document.getElementById("quiz-body").style = "display:none";
+  document.getElementById("startSite").style = "";
+
 }
 
-function questionsArrayLen() {
-  document.getElementById("all-questions").innerHTML = questions.length;
+
+function closeStartSite() {
+  document.getElementById("startSite").style = "display:none";
+  document.getElementById("quiz-body").style = "";
+}
+
+
+function showEndScreen(){
+  if (isOnPage == 1) {
+    showHistoryEndScreen();
+  }
+  if (isOnPage == 2) {
+    showScienceEndScreen();
+  }
+  if (isOnPage == 3) {
+    showSportEndScreen();
+  }
+    AUDIO_FINISH.play();
+}
+
+
+function updateToNextQuestion(){
+  if (isOnPage == 1) {
+    updateToHistory();
+  }
+  if (isOnPage == 2) {
+    updateToScience();
+  }
+  if (isOnPage == 3) {
+    updateToSport();
+  }
+}
+
+
+function updateProgressBar(){
+  if (isOnPage == 1) {
+    historyProgressBar();
+  }
+  if (isOnPage == 2) {
+    scienceProgressBar();
+  }
+  if (isOnPage == 2) {
+    sportProgressBar();
+  }
 }
 
 function showQuestion() {
-  
   if (gameIsOver()) {
     showEndScreen();
   } else {
@@ -51,84 +68,46 @@ function showQuestion() {
   }
 }
 
-function gameIsOver(){
-    return currentQuestion >= questions.length;
-}
-
-function updateToNextQuestion(){
-    let question = questions[currentQuestion];
-    document.getElementById("question-number").innerHTML = currentQuestion + 1;
-    document.getElementById("question-text").innerHTML = question["question"];
-    document.getElementById("answer_1").innerHTML = question["answer_1"];
-    document.getElementById("answer_2").innerHTML = question["answer_2"];
-    document.getElementById("answer_3").innerHTML = question["answer_3"];
-    document.getElementById("answer_4").innerHTML = question["answer_4"];
-}
-
-function updateProgressBar(){
-    let percent = (currentQuestion + 1) / questions.length;
-    percent = Math.round(percent * 100);
-    document.getElementById(`progress-bar`).innerHTML = `${percent}% `;
-    document.getElementById(`progress-bar`).style = `width: ${percent}%;`;
-}
-
-function showEndScreen(){
-    document.getElementById("endScreen").style = "";
-    document.getElementById("quiz-body").style = "display:none";
-    document.getElementById("amountOfQuestions").innerHTML = questions.length;
-    document.getElementById("amountCorrectAnswers").innerHTML = rightQuestion;
-    AUDIO_FINISH.play();
-}
-
 function answer(selection) {
-  if (isOnSciencePage) {
-    let question = scienceQuestions[currentQuestion];
-    let selectedQuestionNumber = selection.slice(-1);
-  let idOfRightAnswer = `answer_${question["right_answer"]}`;
+  if (isOnPage == 1) {
+    handleAnswerForPage(historyQuestions, selection);
+  } else if (isOnPage == 2) {
+    handleAnswerForPage(scienceQuestions, selection);
+  } else if (isOnPage == 3) {
+    handleAnswerForPage(sportQuestions, selection);
+  }
 
-  if (rightAnswerSelected(selectedQuestionNumber,question)) {
-    document.getElementById(selection).parentNode.classList.add("bg-success");
-    rightQuestion++;
-    AUDIO_SUCCESS.play();
-  } else {
-    document.getElementById(selection).parentNode.classList.add("bg-danger");
-    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
-    AUDIO_FAIL.play();
-  }
-  } else {
-    let question = questions[currentQuestion];
-    let selectedQuestionNumber = selection.slice(-1);
-  let idOfRightAnswer = `answer_${question["right_answer"]}`;
-
-  if (rightAnswerSelected(selectedQuestionNumber,question)) {
-    document.getElementById(selection).parentNode.classList.add("bg-success");
-    rightQuestion++;
-    AUDIO_SUCCESS.play();
-  } else {
-    document.getElementById(selection).parentNode.classList.add("bg-danger");
-    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
-    AUDIO_FAIL.play();
-  }
-  }
-  
-  
   document.getElementById("next-button").disabled = false;
 }
+function handleAnswerForPage(questions, selection) {
+  let question = questions[currentQuestion];
+  let selectedQuestionNumber = selection.slice(-1);
+  let idOfRightAnswer = `answer_${question["right_answer"]}`;
+
+  if (rightAnswerSelected(selectedQuestionNumber, question)) {
+    document.getElementById(selection).parentNode.classList.add("bg-success");
+    rightQuestion++;
+    AUDIO_SUCCESS.play();
+  } else {
+    document.getElementById(selection).parentNode.classList.add("bg-danger");
+    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    AUDIO_FAIL.play();
+  }
+}
+
 
 function rightAnswerSelected(selectedQuestionNumber,question){
    return selectedQuestionNumber == question[`right_answer`];
 }
 
+
 function nextQuestion() {
   currentQuestion++;
   document.getElementById("next-button").disabled = true;
   resetAnswerButtons();
-  if (isOnSciencePage) {
-    showScienceQuestion();
-  } else {
-    showQuestion();
-  }
+  showQuestion();
 }
+
 
 function resetAnswerButtons() {
   document.getElementById("answer_1").parentNode.classList.remove("bg-success");
@@ -141,7 +120,20 @@ function resetAnswerButtons() {
   document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
 }
 
+
+function gameIsOver(){
+  if (isOnPage == 1) {
+    return currentQuestion >= historyQuestions.length;
+  } else if (isOnPage == 2) {
+    return currentQuestion >= scienceQuestions.length;
+  } else if (isOnPage == 3) {
+    return currentQuestion >= sportQuestions.length;
+  }
+}
+
+
 function restartGame() {
+  isOnPage =0;
   rightQuestion = 0;
   currentQuestion = 0;
   document.getElementById("endScreen").style = "display:none";
@@ -149,94 +141,74 @@ function restartGame() {
   init();
 }
 
-let scienceQuestions = [
-  {
-    question: "Welche Einheit misst die Intensität von Erdbeben?",
-    answer_1: "Watt",
-    answer_2: "Joule",
-    answer_3: "Hertz",
-    answer_4: "Richter",
-    right_answer: 4,
-    topic: "science",
-  },
-  {
-    question: "Was ist die chemische Formel für Wasser?",
-    answer_1: "CO2",
-    answer_2: "H2O",
-    answer_3: "O2",
-    answer_4: "CH4",
-    right_answer: 2,
-    topic: "science",
-  },
-  {
-    question: "Welches Gas macht etwa 78% der Erdatmosphäre aus?",
-    answer_1: "Sauerstoff",
-    answer_2: "Stickstoff",
-    answer_3: "Kohlendioxid",
-    answer_4: "Wasserstoff",
-    right_answer: 2,
-    topic: "science",
-  },
-  {
-    question: "Was ist die kleinste Einheit eines chemischen Elements?",
-    answer_1: "Molekül",
-    answer_2: "Atom",
-    answer_3: "Isotop",
-    answer_4: "Ion",
-    right_answer: 2,
-    topic: "science",
-  },
-  {
-    question: "Welcher Planet ist der dritte in unserem Sonnensystem?",
-    answer_1: "Mars",
-    answer_2: "Venus",
-    answer_3: "Erde",
-    answer_4: "Jupiter",
-    right_answer: 3,
-    topic: "science",
-  },
-  {
-    question: "Was ist die Geschwindigkeit des Lichts?",
-    answer_1: "300,000 km/s",
-    answer_2: "150,000 km/s",
-    answer_3: "500,000 km/s",
-    answer_4: "1,000,000 km/s",
-    right_answer: 1,
-    topic: "science",
-  },
-];
-function scienceQuestion() {
-  isOnSciencePage = true;
-  let question = scienceQuestions[currentQuestion];
+
+// HISTORY AREA
+
+
+function historyQuestion() {
+  closeStartSite();
+  isOnPage = 1
+  questionsArrayLen();
+  showQuestion();
+}
+
+
+function questionsArrayLen() {
+  document.getElementById("all-questions").innerHTML = historyQuestions.length; 
+}
+
+
+function historyProgressBar() {
+  let percent = (currentQuestion + 1) / historyQuestions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById(`progress-bar`).innerHTML = `${percent}% `;
+    document.getElementById(`progress-bar`).style = `width: ${percent}%;`;
+}
+
+function updateToHistory () {
+  let question = historyQuestions[currentQuestion];
     document.getElementById("question-number").innerHTML = currentQuestion + 1;
     document.getElementById("question-text").innerHTML = question["question"];
     document.getElementById("answer_1").innerHTML = question["answer_1"];
     document.getElementById("answer_2").innerHTML = question["answer_2"];
     document.getElementById("answer_3").innerHTML = question["answer_3"];
     document.getElementById("answer_4").innerHTML = question["answer_4"];
-    scienceQuestionsArrayLen();
-    showScienceQuestion();
+}
+
+
+function showHistoryEndScreen(){
+  document.getElementById("endScreen").style = "";
+      document.getElementById("quiz-body").style = "display:none";
+      document.getElementById("amountOfQuestions").innerHTML = historyQuestions.length;
+      document.getElementById("amountCorrectAnswers").innerHTML = rightQuestion;
+}
+
+
+
+// SCIENCE AREA
+
+
+function scienceQuestion() {
+  closeStartSite();
+  isOnPage = 2;
+  updateToNextQuestion();
+  scienceQuestionsArrayLen();
+  showQuestion();
 }
 
 function scienceQuestionsArrayLen() {
   document.getElementById("all-questions").innerHTML = scienceQuestions.length;
 }
 
-function showScienceQuestion() {
-  if (scienceGameIsOver()) {
-    showScienceEndScreen();
-  } else {
-    updateScienceProgressBar();
-    updateToNextScienceQuestion();
-  }
+
+function scienceProgressBar(){
+  let percent = (currentQuestion + 1) / scienceQuestions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById(`progress-bar`).innerHTML = `${percent}% `;
+    document.getElementById(`progress-bar`).style = `width: ${percent}%;`;
 }
 
-function scienceGameIsOver(){
-    return currentQuestion >= scienceQuestions.length;
-    console.log("scienceQuestion")
-}
-
-function updateToNextScienceQuestion(){
+function updateToScience(){
     let question = scienceQuestions[currentQuestion];
     document.getElementById("question-number").innerHTML = currentQuestion + 1;
     document.getElementById("question-text").innerHTML = question["question"];
@@ -246,17 +218,51 @@ function updateToNextScienceQuestion(){
     document.getElementById("answer_4").innerHTML = question["answer_4"];
 }
 
-function updateScienceProgressBar(){
-    let percent = (currentQuestion + 1) / scienceQuestions.length;
+function showScienceEndScreen(){
+  document.getElementById("endScreen").style = "";
+    document.getElementById("quiz-body").style = "display:none";
+    document.getElementById("amountOfQuestions").innerHTML = scienceQuestions.length;
+    document.getElementById("amountCorrectAnswers").innerHTML = rightQuestion;
+}
+
+
+//  SPORT AREA
+
+
+function sportQuestion() {
+  closeStartSite();
+  isOnPage = 3;
+  updateToNextQuestion();
+  sportQuestionsArrayLen();
+  showQuestion();
+}
+
+function sportQuestionsArrayLen() {
+  document.getElementById("all-questions").innerHTML = sportQuestions.length;
+}
+
+function updateToSport(){
+    let question = sportQuestions[currentQuestion];
+    document.getElementById("question-number").innerHTML = currentQuestion + 1;
+    document.getElementById("question-text").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+
+function sportProgressBar(){
+  let percent = (currentQuestion + 1) / sportQuestions.length;
     percent = Math.round(percent * 100);
     document.getElementById(`progress-bar`).innerHTML = `${percent}% `;
     document.getElementById(`progress-bar`).style = `width: ${percent}%;`;
 }
 
-function showScienceEndScreen(){
-    document.getElementById("endScreen").style = "";
+
+function showSportEndScreen(){
+  document.getElementById("endScreen").style = "";
     document.getElementById("quiz-body").style = "display:none";
-    document.getElementById("amountOfQuestions").innerHTML = scienceQuestions.length;
+    document.getElementById("amountOfQuestions").innerHTML = sportQuestions.length;
     document.getElementById("amountCorrectAnswers").innerHTML = rightQuestion;
-    AUDIO_FINISH.play();
 }
